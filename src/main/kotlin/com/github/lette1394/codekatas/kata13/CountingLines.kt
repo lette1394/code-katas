f8ca1c395a0cea804e2d5fe8b3584b8b633baa26
lette1394 <lette1394@gmail.com>
2024-02-24 22:57:20 +0900

    kata13) 주석은 라인 수로 세지 않는다
    Signed-off-by: lette1394 <lette1394@gmail.com>

--- src/main/kotlin/com/github/lette1394/codekatas/kata13/CountingLines.kt
+++ src/main/kotlin/com/github/lette1394/codekatas/kata13/CountingLines.kt
@@ -4,7 +4,9 @@ class CountingLines(
     private val javaCode: String,
 ) {
     fun count(): Long {
-        if (javaCode.isNotBlank()) {
+        if (javaCode.contains("//")) {
+            return javaCode.lines().size.toLong() - 1
+        } else if (javaCode.isNotBlank()) {
             return javaCode.lines().size.toLong()
         } else {
             return 0L
